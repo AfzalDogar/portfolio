c53275df8b857e9db2420d7733f427a44a025695
unknown <dogard866@gmail.com>
2024-10-01 12:59:07 +0500

    portfolio

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="bg-[#22a6b3] py-1 sm::py-2">
+        <p className="text-center text-[#27272c] text-[11px] md:text-lg">
+          Copyright &copy; 2024, Made with ❤ by Afzal Dogar.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
