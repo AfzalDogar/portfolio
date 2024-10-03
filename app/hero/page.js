713b3b8eb3f3a0800af71b8462f468351830ca02
unknown <dogard866@gmail.com>
2024-10-03 15:38:30 +0500

    portfolio

--- app/hero/page.js
+++ app/hero/page.js
@@ -12,7 +12,8 @@ const Home = () => {
           <div className="text-center lg:text-left order-1 lg:order-none">
             <span className="text-xl text-accent">Frontend Developer</span>
             <h1 className="h1 mb-6">
-              Hello I&apos;m <br /> <span className="text-accent">Afzal</span>
+              Hello I&apos;m <br />{" "}
+              <span className="text-accent">Afzal Dogar </span>
             </h1>
             <p className="max-w-[500px] text-white mb-9">
               Expert Front-end Developer with 2+ years of expertise in crafting
